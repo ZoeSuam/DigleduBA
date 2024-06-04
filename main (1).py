cddc9de087bc79151021915b8cc739b1515e95a2
ZoeSuam <lena_zoe.maus@smail.th-koeln.de>
2024-06-04 14:22:05 +0200

    deleted Secrets

--- main (1).py
+++ main (1).py
@@ -15,7 +15,7 @@ import logging
 required_version = version.parse("1.1.1")
 
 current_version = version.parse(openai.__version__)
-OPENAI_API_KEY = 'secret'
+OPENAI_API_K = 'secret'
 if current_version < required_version:
   raise ValueError(f"Error: OpenAI version {openai.__version__}"
                    " is less than the required version 1.1.1")
